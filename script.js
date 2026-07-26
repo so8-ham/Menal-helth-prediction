7cce9b85ab8b977b279825bad6f06f4b8e3111a3
soham <sohammandal605@gmail.com>
2026-07-26 20:21:38 +0530

    deply link add  in js file

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "http://127.0.0.1:8000"
+  const API_BASE = "https://menal-helth-prediction-1.onrender.com"
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
